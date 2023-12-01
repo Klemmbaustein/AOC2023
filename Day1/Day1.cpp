36316c45e3f6b9f4c48d6e76c2697c742d0b1bbd
Klemmbaustein <83748124+Klemmbaustein@users.noreply.github.com>
2023-12-01 15:22:30 +0100

    removed unused function

--- Day1/Day1.cpp
+++ Day1/Day1.cpp
@@ -20,17 +20,6 @@ std::unordered_map<string, string> DigitStrings =
 };
 #endif
 
-// https://stackoverflow.com/questions/2896600/how-to-replace-all-occurrences-of-a-character-in-string
-void ReplaceAll(string& str, const string& from, const string& to) 
-{
-	size_t start_pos = 0;
-	while ((start_pos = str.find(from, start_pos)) != string::npos) 
-	{
-		str.replace(start_pos, from.length(), to);
-		start_pos += to.length(); // Handles case where 'to' is a substring of 'from'
-	}
-}
-
 int main()
 {
 #if INPUT_TEST
